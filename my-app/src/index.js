1fc288194102bc2e38c3004553bfc2efee602614
Jorge Alexander Taberoa Jimenez <taberoajorge@pm.me>
2022-02-14 10:30:59 -0600

    literal objects

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,28 @@
-const name = 'Jorge';
-const lastname = 'Taberoa';
+const person = {
+  name: "Jorge",
+  lastname: "Taberoa",
+  age: 21,
+  address: {
+    city: 'El Tigre',
+    zp: 6060,
+    lat: -14.323,
+    lng: 34.124,
+  }
+};
 
-// const fullName = name+ ' '+ lastname;
-const fullName = `${name} ${lastname}`;
+// console.table(person);
+console.log(person);
 
-console.log(fullName);
+/* Esto no lo sabia, esa asignacion no es un 
+un objeto nuevo, es una referencia al objeto
+principal, por lo que si modifico person2
+Se va a modificar el valor de person*/
+const person2 = person;
+// person2.name = 'Chale';
+
+// Esto se evita creando un nuevo objeto y usando el spread operator
+const person3 = {...person};
+person3.name = 'Chale';
+
+
+console.log(person3);
