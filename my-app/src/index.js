0aeb0e9090478be7d977fb83f1702f98e563f2d4
Jorge Alexander Taberoa Jimenez <taberoajorge@pm.me>
2022-02-14 10:16:26 -0600

    literal strings

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,7 @@
-// Variables y constantes
-
 const name = 'Jorge';
 const lastname = 'Taberoa';
 
-let gotValue = 5;
-gotValue = 4;
+// const fullName = name+ ' '+ lastname;
+const fullName = `${name} ${lastname}`;
 
-console.log(name, lastname, gotValue);
-
-// Don't use 'Var'...
-if (true) {
-  const name = 'Other name';
-  console.log(name);
-}
-
-console.log(gotValue);
+console.log(fullName);
