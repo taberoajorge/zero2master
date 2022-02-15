70f29ca311c451d6daf5de67fbea43e3dc081f01
taberoajorge <taberoajorge@pm.me>
2022-02-15 15:15:17 -0600

    Promesas

---
+++ my-app/src/data/functions.js
@@ -0,0 +1,12 @@
+import {heroes} from "./heros";
+
+const heroe = (id) => {
+  return heroes.find((item) => id === item.id);
+};
+
+const moviesPerOwner = (owner) => heroes.filter((item)=> item.owner === owner)
+
+export  { 
+  heroe,
+  moviesPerOwner 
+}; 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,29 @@
-import {heroes} from "./data/heros";
+import { heroe, moviesPerOwner } from './data/functions';
 
-const heroe = (id) => {
-  return heroes.find((item) => id === item.id);
+// const promise = new Promise( (resolve, reject) => {
+
+//   setTimeout(() => {
+//     const data = moviesPerOwner('DC');
+//     resolve(data)
+//   }, 3000);
+
+// });
+
+// promise
+//   .then(data => console.log(data))
+
+const getHeroeByIdAsync = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = heroe(id);
+      data
+        ? resolve(data)
+        : reject('No se encontro el heroe')
+
+    }, 3000);
+  });
 };
 
-const moviesPerOwner = (owner) => heroes.filter((item)=> item.owner === owner)
-
-console.log(heroe(5));
-console.log(moviesPerOwner('DC'));
+getHeroeByIdAsync(2)
+  .then( console.log )
+  .catch( console.warn )
