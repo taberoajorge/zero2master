18b06de323c3434d326f7fb25e7b5aa8935d2706
taberoajorge <taberoajorge@pm.me>
2022-02-16 16:12:58 -0600

    Events

--- counter-app/src/CounterApp.js
+++ counter-app/src/CounterApp.js
@@ -2,10 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 function CounterApp({ value }) {
+
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+      <button onClick={handleAdd}>+</button>
     </>
   )
 }
