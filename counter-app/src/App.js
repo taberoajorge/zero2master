512ab681525c8a195b6d3e7e6ad3dfbcb56bdd92
taberoajorge <taberoajorge@pm.me>
2022-02-16 15:12:22 -0600

    PropTypes

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const App = () => {
+
+const App = ({ saludo }) => {
+
     return (
         <>
-            <h1> Hello world! </h1>
+            <h1>{saludo}</h1>
+            {/* <h1> {JSON.stringify(greeting, null, 5)} </h1> */}
+            <p>My first App</p>
         </>
     );
 };
+
+App.propTypes = {
+    saludo: PropTypes.string.isRequired,
+    other: PropTypes.number.isRequired,
+}
 
 export default App;
