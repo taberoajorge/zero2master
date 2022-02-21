6ab0283ae6551614f75dc53ad69fcc992a7be930
taberoajorge <taberoajorge@pm.me>
2022-02-21 17:06:19 -0600

    Add useEffecto to handle forms

--- hook-app/src/App.js
+++ hook-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 // import CounterApp from './components/01-useState/CounterApp';
-import CounterAppWithHook from './components/01-useState/CounterAppWithHook';
+// import CounterAppWithHook from './components/01-useState/CounterAppWithHook';
+import SimpleForm from './components/02-useEffect/SimpleForm';
 
 function App() {
   return (
     <>
       <h1>App</h1>
-      <CounterAppWithHook />
+      <SimpleForm />
     </>
   )
 }
