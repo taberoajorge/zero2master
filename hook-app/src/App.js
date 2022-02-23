93ea0090c28704a1d7f375793a286fe4715a769e
taberoajorge <taberoajorge@pm.me>
2022-02-23 14:10:00 -0600

    Tarea

--- hook-app/src/App.js
+++ hook-app/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import CallbackHook from './components/04-memos/CallbackHook';
+// import CallbackHook from './components/04-memos/CallbackHook';
 // import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 // import MultipleHooks from './components/03-example/MultipleHooks';
-import { MemoHook } from './components/04-memos/MemoHook';
+// import { MemoHook } from './components/04-memos/MemoHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterAppWithHook from './components/01-useState/CounterAppWithHook';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <h1>App</h1>
-      <CallbackHook />
+      <Padre />
     </>
   )
 }
